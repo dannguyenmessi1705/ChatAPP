45468a1f5040a92b70f17fb1bd1c3e26eb8539d1
dannguyenmessi1705 <github.com/dannguyenmessi1705>
2023-05-16 05:50:04 +0700

    ProChat

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,8 @@
-export const login = "https://api-pro-chat.onrender.com/api/token";
-export const register = "https://api-pro-chat.onrender.com/api/register";
-export const get_user_from_token = "https://api-pro-chat.onrender.com/api/verify";
-export const twilio = "https://api-pro-chat.onrender.com/api/twilio/token";
+export const login = "http://localhost:8000/api/token";
+export const register = "http://localhost:8000/api/register";
+export const get_user_from_token = "http://localhost:8000/api/verify";
+export const twilio = "http://localhost:8000/api/twilio/token";
 export const upload_profile_pic =
-  "https://api-pro-chat.onrender.com/api/user/profile_picture";
+  "http://localhost:8000/api/user/profile_picture";
+
+
--- client/src/api/rooms.js
+++ client/src/api/rooms.js
@@ -1,5 +1,5 @@
-export const get_rooms = "https://api-pro-chat.onrender.com/api/rooms";
-export const get_room = "https://api-pro-chat.onrender.com/api/room";
-export const create_room = "https://api-pro-chat.onrender.com/api/room";
-export const put_user_into_room = "https://api-pro-chat.onrender.com/api/room";
-export const favorites = "https://api-pro-chat.onrender.com/api/favorites";
+export const get_rooms = "http://localhost:8000/api/rooms";
+export const get_room = "http://localhost:8000/api/room";
+export const create_room = "http://localhost:8000/api/room";
+export const put_user_into_room = "http://localhost:8000/api/room";
+export const favorites = "http://localhost:8000/api/favorites";
